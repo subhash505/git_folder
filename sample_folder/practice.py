d79288177a3483921ea3009aa0c4da3d360b27c9
Subhash Sharma <subhash.sharma@ezops.com>
2023-05-09 11:57:08 +0530

    update in practice

--- sample_folder/practice.py
+++ sample_folder/practice.py
@@ -10,12 +10,20 @@ def main():
     # import configparser
     parser = configparser.ConfigParser()
     parser.add_section('Manager')
-    parser.set('Manager', 'Name', 'Ashok Kulkarni')
-    parser.set('Manager', 'email', 'ashok@gmail.com')
+    parser.set('Manager', 'Name', 'subhash sharma')
+    parser.set('Manager', 'email', 'mr.subhash505@gmail.com')
     parser.set('Manager', 'password', 'secret')
     fp=open('test.ini','w')
     parser.write(fp)
     fp.close()
+    # p= parser.get()
+    # print(p)
+
+    r = 0
+    s = [59, 47, 7, 17, 45]
+    l  = lambda s: sum(s)
+    print(l(s))
+
 
 if __name__ == "__main__" :
     main()
