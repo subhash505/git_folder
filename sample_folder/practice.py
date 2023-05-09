6a6f91873c2f7e70bc276e36033cddc557309286
Subhash Sharma <subhash.sharma@ezops.com>
2023-05-09 09:54:59 +0530

    config parser

--- sample_folder/practice.py
+++ sample_folder/practice.py
@@ -1,12 +1,21 @@
 import configparser
 def main():
     parser = configparser.ConfigParser()
-    parser.read('sampleconfig.ini')
+    parser.read('test.ini')
     for sect in parser.sections():
         print('Section:', sect)
         for k,v in parser.items(sect):
             print(' {} = {}'.format(k,v))
         print()
+    # import configparser
+    parser = configparser.ConfigParser()
+    parser.add_section('Manager')
+    parser.set('Manager', 'Name', 'Ashok Kulkarni')
+    parser.set('Manager', 'email', 'ashok@gmail.com')
+    parser.set('Manager', 'password', 'secret')
+    fp=open('test.ini','w')
+    parser.write(fp)
+    fp.close()
 
 if __name__ == "__main__" :
     main()
